4401633ceec7cff67d5110df075cf096a4f1030c
Dobrohotova <irina.dobrohotova@gmail.com>
2017-05-03 22:55:16 +0200

    Footer fixed, but why when I add on home page it does changes on the header as well?

--- index.php
+++ index.php
@@ -307,6 +307,9 @@ $oops = $query_oop_projects->fetchAll(PDO::FETCH_ASSOC);
 		<div class="map">
 			
 		</div>
+		<!-- <div class="footer">
+			<?php include('footer.html'); ?>
+		</div> -->
 	</div>
 </body>
 </html>
