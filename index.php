2ddf50a4289d13f4435fef5dc96fa04b8c99e274
Dobrohotova <irina.dobrohotova@gmail.com>
2017-05-03 11:28:09 +0200

    Index page php fixed

--- index.php
+++ index.php
@@ -209,18 +209,14 @@ $oops = $query_oop_projects->fetchAll(PDO::FETCH_ASSOC);
 				</div>
 				<div class="oop-proekti">
 					<?php foreach ($oops as $key => $oop){
-						if($key < 3){
-							$class = "oop2";
-						} else {
-							$class = "oop4";
+						$class = '';
+						if($key == 1){
+							$class = 'class="oop2"';
+						} else if($key == 3) {
+							$class = 'class="oop4"';
 						}
 					 ?>
-						<img class="<?= $class ?>" src="<?=$oop['photo']?>">
-					<!-- <img src="media/images/oop1.png">
-					<img class="oop2" src="media/images/oop2.png">
-					<img src="media/images/oop3.png">
-					<img class="oop4" src="media/images/oop4.png">
-					<img src="media/images/oop5.png"> -->
+						<img <?= $class ?> src="<?=$oop['photo']?>">
 					<?php } ?>
 				</div>
 			</div>
