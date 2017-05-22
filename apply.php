53564949096f4fdd2ad6e09ebd14a5bdf92f84d6
Stefan <stefanblazevski226@gmail.com>
2017-05-22 19:13:56 +0200

    Apply Fix

--- apply.php
+++ apply.php
@@ -9,7 +9,7 @@
 	<?php include 'header_all.html'; ?>
 	<div class="container">
 		<div class= "picholder">
-			<div class= "pic"><img src="media/images/toppic.png"></div>
+			<div class= "pic"><img src="media/images/toppic.png" width="100%"></div>
 		</div>
 		<div class="content">
 			<div class="text">
@@ -20,39 +20,39 @@
 			</div>
 			<div class= "content2">
 				<form action="apply.php" method="post">
-				<div class="inputime">
+				<div class="inputs">
 					<div class>
-						<h1 class="ime">Име</h1>
+						<p class="inputfields">Име</p>
 						<input type="text" name="ime">
 					</div>
 				</div>
-				<div class="inputime">
+				<div class="inputs">
 					<div class>
-						<h1 class="prezime">Презиме</h1>
+						<p class="inputfields">Презиме</p>
 						<input type="text" name="prezime">
 					</div>
 				</div>
-				<div class="inputime">
+				<div class="inputs">
 					<div class>
-						<h1 class="prezime">Телефон</h1>
-						<input type="text" name="telefon">
+						<p class="inputfields">Телефон</p>
+						<input type="number" name="telefon">
 					</div>
 				</div>
-				<div class="inputime">
+				<div class="inputs">
 					<div class>
-						<h1 class="prezime">E-mail</h1>
+						<p class="inputfields">E-mail</p>
 						<input type="email" name="email">
 					</div>
 				</div>
-				<div class="kopce"><button class= "potvrdi" type="submit">Потврди</button></div>
+				<div class="kopce"><button class= "potvrdi" type="submit">потврди</button></div>
 				</form>
-				<div class="slika">
-					<img src= "media/images/picture.png">
+				<div class="bottompic">
+					<img src= "media/images/picture.png" width="100%">
 				</div>
 			</div>
 			<div class="mapholder">
 				<div class= "map">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2965.548811988407!2d21.449518215513486!3d41.98849597921361!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x135415bfdd371667%3A0xccd976215f845b88!2sSemos+Computer+Education+Center!5e0!3m2!1sen!2smk!4v1491147640805" width="1920" height="1420" frameborder="0" style="border:0" allowfullscreen></iframe>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2965.548811988407!2d21.449518215513486!3d41.98849597921361!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x135415bfdd371667%3A0xccd976215f845b88!2sSemos+Computer+Education+Center!5e0!3m2!1sen!2smk!4v1491147640805" width="100%" height="900" frameborder="0" style="border:0" allowfullscreen></iframe>
 				</div>
 			</div>
 		</div>
